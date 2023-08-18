32947cb32049e008ecbf95739506d2a3fc920f72
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2023-08-18 15:30:35 +0900

    8분 삭제

--- camera.py
+++ camera.py
@@ -16,12 +16,14 @@ class VideoCamera(object):
     def get_frame(self):
         success, image = self.video.read()
         image = cv2.resize(image, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
+        sql = "insert into vsAlarm (siteID,alarmType,locationX,locationY,alarmTime) values ('100001','FIRE',%u,%u,now())" % (0, 0)
+        dbins(sql)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         fire = fire_cascade.detectMultiScale(image, 12, 5)
         for (x, y, w, h) in fire:
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            sql = "insert into vsAlarm (siteID,alarmType,locationX,locationY,alarmTime) values ('100001','FIRE',%u,%u,now())" % (x, y)
-            dbins(sql)
+#            sql = "insert into vsAlarm (siteID,alarmType,locationX,locationY,alarmTime) values ('100001','FIRE',%u,%u,now())" % (x, y)
+#            dbins(sql)
             break
 
         ret, jpeg = cv2.imencode('.jpg', image)
